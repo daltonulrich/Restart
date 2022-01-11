141fac6b9c577f15043c579b27cb4a3e1ff2534d
Dalton <daltonulrich2002@gmail.com>
2022-01-11 10:29:14 -0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Dalton Ulrich on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
